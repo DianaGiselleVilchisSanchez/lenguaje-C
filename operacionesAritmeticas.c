6aa11ff85f65144463df581c930a5924ca68f949
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-04-30 23:56:07 -0600

    Arithmetic operations
    In the main method, write the code to print the sum of 5 + 8, and 78787 + 3259.

---
+++ operacionesAritmeticas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Ejercicio: en el metodo principal (main) 
+escribir el codigo para imprimir la suma de 
+5 + 8
+78787 + 3259*/
+
+int main(){
+    printf ("%d\n", 5 + 6);
+    printf ("%d\n", 78787 + 3259);
+    return 0;
+}
