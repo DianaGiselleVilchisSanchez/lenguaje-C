545928deeff089d0ae557a167f449418fd17e041
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-05-02 19:18:43 -0600

    Circle in C++
    Calculate the area of a circle that is 10 meters in diameter.

---
+++ circulo.c
@@ -0,0 +1,9 @@
+/* Crear un programa que calcule el area de un circulo que tiene 10 metros de diametro*/
+#include <stdio.h>
+
+int main (){
+    float diametro = 10;
+    float pi = 3.1416;
+    printf ("El area del circulo es: %f", diametro*pi);
+    return 0;
+}
