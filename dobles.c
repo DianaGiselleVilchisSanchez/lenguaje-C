6e41f22833e8f7b0ff4999dd0293b4cd5fbb431e
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-05-02 19:27:59 -0600

    Declare two double (real) variables.
    Declare two double (real) variables and assign them values different from zero.
    And show the result of dividing the first one between the second.

---
+++ dobles.c
@@ -0,0 +1,11 @@
+/* Declarar 2 variables dobles (reales) y asignarles valores diferentes de cero
+Mostar el resultado de dividir la primera entre la segunda */
+
+#include <stdio.h>
+int main (){
+    double num1, num2;
+    num1 = 14.5;
+    num2 = 2.5;
+    printf ("El resultado es: %f", num1/num2);
+    return 0;
+}
