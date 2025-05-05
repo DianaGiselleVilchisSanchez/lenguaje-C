b7b90de72d8fadb17d4ae9dc69fffcce43d7960e
DianaGiselleVilchisSanchez <dianavilsan2004@gmail.com>
2025-05-05 14:00:00 -0600

    Reproductor de notas nusicales

---
+++ codigo55.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    Beep(262, 300); // DO
+    Beep(294, 300); // RE
+    Beep(330, 300); // MI
+    Beep(349, 300); // FA
+    Beep(392, 300); // SOL
+    Beep(440, 300); // LA
+    Beep(494, 300); // SI
+    Beep(523, 600); // DO
+    return 0;
+}
